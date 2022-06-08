daa9ba5bfe9514cfee8de3bffbf740455e49ef5b
Calvin Grimm <calvinjgrimm@gmail.com>
2022-06-08 15:26:19 -0600

    Adding plot to random number generation. Reading Mauna Loa data and creating plot from data.

--- lmExample.R
+++ lmExample.R
@@ -7,3 +7,13 @@ y = x + rnorm(100, mean = 0, sd = 2)
 fit = lm(y ~ x)
 
 summary(fit)
+
+#Create plot from Random Numbers.
+plot(x,y)
+abline(fit, col=2)
+
+#Read and plot from Mauna Loa file
+co2 = read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
